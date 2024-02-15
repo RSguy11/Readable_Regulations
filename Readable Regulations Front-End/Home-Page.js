2856222035f70ef7f520a2f5b5c5190ae553a05e
Sean Lawrence <85978841+RSguy11@users.noreply.github.com>
2024-02-15 11:32:46 -0500

    Add files via upload

---
+++ Readable Regulations Front-End/Home-Page.js
@@ -0,0 +1,86 @@
+document.getElementById("submit-button").addEventListener("click",submitForm);
+
+
+
+document.getElementById("input-results-box").addEventListener("click", function(event) {
+    if (event.target.id === "label") {
+        switchPage();
+    }
+    if (event.target.id === "box-click"){
+        expantion();
+    }
+});
+
+
+document.getElementById("nav-bar").addEventListener("click", function(event) {
+    if (event.target.id === "economic") {
+        ClickedHighlight("economic");
+    }
+    if (event.target.id === "defence"){
+        ClickedHighlight("defence");
+    }
+    if (event.target.id === "environmental"){
+        ClickedHighlight("environmental");
+    }
+    if (event.target.id === "transportation"){
+        ClickedHighlight("transportation");
+    }
+    if (event.target.id === "other"){
+        ClickedHighlight("other");
+    }
+});
+
+
+
+function ClickedHighlight(Eid){
+    var ele1 = document.getElementById(Eid);
+    
+    if (ele1.classList.contains("clickedHighlight")){
+        ele1.classList.remove("clickedHighlight");
+    }else{
+        ele1.classList.add("clickedHighlight");
+
+    }
+}
+
+
+// function test(){
+//     document.getElementById("three").innerText = "hello";
+// }
+
+function switchPage(){
+    window.location.href ='AdvancedPage.html';
+}
+
+function expantion(){
+    var inputbox = document.getElementById("label-");
+
+}
+
+
+function submitForm() {
+    var inputbox = document.getElementById("input-results-box");
+
+
+    // Get the value from the input field
+    var linkValue = document.getElementById("link-ins").value;
+    var box = document.createElement("div");
+    box.classList.add("box")
+    box.id = "box-click";
+
+    var label = document.createElement("label");
+    label.innerText = "Submitted Link: " + linkValue;
+    label.classList.add("box-inner-text");
+    label.id = "label"
+
+    box.appendChild(label);
+
+    inputbox.appendChild(box);
+
+    // document.getElementById("three").innerText = box.id;
+
+    
+
+}
+
+
